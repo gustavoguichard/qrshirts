95e3d189881d013ad33d63d71366a482890bf61f
Gustavo Guichard <gustavoguichard@gmail.com>
2013-03-02 14:52:06 -0300

    Translating and converting from erb to slim

--- config/application.rb
+++ config/application.rb
@@ -37,11 +37,12 @@ module Hadean
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # "Pacific Time (US & Canada)"
     # 'Central Time (US & Canada)'
-    config.time_zone = 'Eastern Time (US & Canada)'
+    config.time_zone = 'Brasilia'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = "pt-BR"
+    config.i18n.locale = "pt-BR"
 
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
