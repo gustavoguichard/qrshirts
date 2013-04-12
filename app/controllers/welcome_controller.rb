0e40c8cd70b27b7577ac3b584b94f5c90c3deece
Gustavo Guichard <gustavoguichard@gmail.com>
2013-04-12 19:00:18 -0300

    Adjustments in home

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   def index
     @banners = Banner.active
     @featured_product = Product.featured
-    @best_selling_products = Product.active.limit(4).includes(:variants)
+    @best_selling_products = Product.active.sample(4).includes(:variants)
     unless @featured_product
       if current_user && current_user.admin?
         redirect_to admin_merchandise_products_url
