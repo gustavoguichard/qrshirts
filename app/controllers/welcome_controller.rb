3c04347fe866b2abeba83722da55718aae3f038c
Gustavo Guichard <gustavoguichard@gmail.com>
2013-04-12 18:47:45 -0300

    Shows on home just active products

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   def index
     @banners = Banner.active
     @featured_product = Product.featured
-    @best_selling_products = Product.limit(4)
+    @best_selling_products = Product.active.limit(4).includes(:variants)
     unless @featured_product
       if current_user && current_user.admin?
         redirect_to admin_merchandise_products_url
